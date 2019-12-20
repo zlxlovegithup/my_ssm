0908d8056608a3f02323e309a562902ab27f1c78
zlx <3371994757@qq.com>
2019-12-20 13:32:07 +0800

    这是一个简单的ssm整合项目

--- Exercises/src/main/java/com/zlx/dao/ICustomerDao.java
+++
@@ -1,6 +0,0 @@
-package com.zlx.dao;
-import com.zlx.domain.Customer;
-
-public interface ICustomerDao {
-	Customer getCustomerByAccount(Customer customer);
-}
--- Exercises/src/main/java/com/zlx/service/ICustomerService.java
+++
@@ -1,7 +0,0 @@
-package com.zlx.service;
-
-import com.zlx.domain.Customer;
-
-public interface ICustomerService{
-    Customer getCustomerByAccount(Customer customer);
-}
